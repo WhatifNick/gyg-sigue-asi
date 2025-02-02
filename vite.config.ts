7659626ba01d173172d66a492b2061d2aa849fb0
Nick Pasquariello <36612432+WhatifNick@users.noreply.github.com>
2025-02-02 16:09:42 +1000

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react';
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  // base: '/gyg-sigue-asi/',
+  base: '/gyg-sigue-asi/',
   resolve: {
     alias: {
       src: '/src',
